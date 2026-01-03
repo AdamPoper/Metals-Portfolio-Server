ee26eab702d0193ef8cfd10b95bec9d1c87636ed
Adam Poper <adampoper@MacBookPro.lan>
2026-01-03 17:58:59 -0500

    adding route to fetch time series data by a date range

--- src/routes/time-series-routes.ts
+++ src/routes/time-series-routes.ts
@@ -4,6 +4,7 @@ import timeSeriesController from '../controllers/time-series-controller';
 const router = express.Router();
 
 router.get('/all', timeSeriesController.getAllTimeSeriesData);
+router.get('/date-range', timeSeriesController.getTimeSeriesDataByDateRange);
 router.post('/add', timeSeriesController.addSnapshotToTimeSeries);
 
 export default router;
